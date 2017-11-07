c64b789ba9bf689b29d39f8f73c6b770704eda6f
sxoxs <33289095+sxoxs@users.noreply.github.com>
2017-11-07 12:54:48 +0300

    изменил, так как обычный Arrays.sort сортировал только по значениям, а не по длинне строк

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,25 +4,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	    String[] strArray = new String[5];
+        String[] strArray = new String[5];
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите 5 строк: ");
         for (int i = 0; i < strArray.length; i++) {
-            strArray[i] = inConsole();
+            strArray[i] = br.readLine();
         }
-        Arrays.sort(strArray);
-        System.out.println("\nСортированный массив строк: ");
-        for (String i : strArray) {
-            System.out.println(i);
-        }
-    }
 
-    static String inConsole() throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        return br.readLine();
+        Comparator<String> stringLengthComparator = new StringLengthSort();
+        Arrays.sort(strArray, stringLengthComparator);
+
+        System.out.println("\nСортированный массив строк: ");
+        for (String line : strArray) {
+            System.out.println(line);
+        }
     }
 }
